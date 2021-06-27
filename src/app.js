394f074a033fec429a3431c74f2aae4691268c43
littlejane <janeramerindo@gmail.com>
2021-06-27 12:02:48 -0300

    fazendo um teste simples

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express =  require('express')
 const cors = require('cors')
-const database = require('./data/database')
+// const database = require('./data/database')
 
 /** ROTAS */
-const host = require('./routes/host')
-const adress = require('./routes/adress')
-const colaboradoras = require('./routes/colaboradoras')
-const documentation = require('./routes/doc')
+// const host = require('./routes/host')
+// const adress = require('./routes/adress')
+// const colaboradoras = require('./routes/colaboradoras')
+// const documentation = require('./routes/doc')
 
 const app = express();
 
@@ -14,12 +14,14 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-database.connect();
+// database.connect();
 
-app.use("/cantinho", host)
-app.use("/localizacao", adress)
-app.use("/admin", colaboradoras)
-app.use("/doc", documentation)
+// app.use("/cantinho", host)
+// app.use("/localizacao", adress)
+// app.use("/admin", colaboradoras)
+// app.use("/doc", documentation)
+
+app.get("/", (req, res) => res.status(200).json({ message : "funcionando"}))
 module.exports = app;
 
 
