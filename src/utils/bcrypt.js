9522723515e038fd194b9135fe3d6ef5de29d4f5
Beatriz-Ramerindo <janeramerindo@gmail.com>
2021-07-14 19:12:34 -0300

    corrigindo o bcript

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -3,9 +3,6 @@ const bcrypt = require('bcrypt')
 
 module.exports = {
     hashPassword(password, salt=7 /* is Default 7 */) {
-
+         return bcrypt.hashSync(password, salt);
     },
-    comparePassword() {
-
-    }
 }
